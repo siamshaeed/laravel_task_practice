4e2fa461ca33aa3a30f141a791c286fe63f81c94
siamshaeed <siamshaeed@gmail.com>
2021-09-23 16:18:36 +0600

    Intervention image resize image

--- app/Http/Controllers/ImageInterventionController.php
+++ app/Http/Controllers/ImageInterventionController.php
@@ -18,8 +18,11 @@ class ImageInterventionController extends Controller
         $imageName = $image->getClientOriginalName();
         $directory = 'blog-image/' . $imageName;
 
-        //Intervention Image simple upload done
-        $resize_image = Image::make($image)->save(public_path($directory));
+        //Intervention Image simple upload
+        // $resize_image = Image::make($image)->save(public_path($directory));
+
+        //Intervention Image with resize upload
+        // $resize_image = Image::make($image)->resize(100,100)->save(public_path($directory));
 
          $dbImage = new Invtimage();
          $dbImage->imageField   =  $directory;
