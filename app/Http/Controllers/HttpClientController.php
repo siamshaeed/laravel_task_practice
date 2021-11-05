47f8630aaf6fd68bcba2f05b245b620f4f5aee60
siamshaeed <siamshaeed@gmail.com>
2021-11-05 20:05:31 +0600

    http client and API

--- app/Http/Controllers/HttpClientController.php
+++ app/Http/Controllers/HttpClientController.php
@@ -13,19 +13,6 @@ class HttpClientController extends Controller
         return $showAlldata->json();
     }
 
-    // public function httpSinglePost($id)
-    // {
-    //     $showSingleData = Http::get('https://jsonplaceholder.typicode.com/posts');
-    //     $test = $showSingleData->json();
-    //     //return $test[0]['id'];
-
-    //     for ($i = 0; $i < count($test); $i++) {
-    //         if ($test[$i]['id'] == $id) {
-    //             return $test[$i];
-    //         }
-    //     }
-    // }
-
     public function addData()
     {
         $addData = Http::post('https://jsonplaceholder.typicode.com/posts', [
